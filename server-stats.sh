37a0830cc8eebe345ac81b50136d044c54bafafa
parwezalam9582 <parwezalam9582@gmail.com>
2025-07-29 15:33:10 +0000

    CPU Usage

--- server-stats.sh
+++ server-stats.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "Total CPU usage"
-echo "Hello"
+# Get total CPU usage using top and awk
+echo "Total CPU usage:"
+
+#Parse CPU idle value and subtract from 100%
+top -bn1 | grep "Cpu(s)" | \
+awk '{usage = 100 - $8; printf "CPU Usage: %.2f%%\n", usage}'
+
 
